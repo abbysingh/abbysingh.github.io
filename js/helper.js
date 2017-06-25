45f9fc3db9b5afdf15eb6a52e6129aad4e7c8cd5
Abheyraj <abheyrajsingh@gmail.com>
2017-06-25 10:56:18 +0530

    New Website!
    Updated to the new website and moved the old one to a subfolder

---
+++ js/helper.js
@@ -0,0 +1,68 @@
+function showProjectDetails(projectName) {
+    var projectContentContainer = $('.project-content-container');
+    projectContentContainer.empty();
+//    var tilt = document.querySelector("#"+projectName);
+//    console.log(tilt);
+    var projectDisplay = anime({
+        targets: '.project-display',
+        duration: 600,
+        top: '0px',
+        easing: 'easeInOutCirc',
+    });
+    projectDisplay.complete = function(){
+        projectContentContainer.load('./projects/' + projectName + '.html');
+    }
+//    tilt.vanillaTilt.reset();
+//    tilt.tilt.reset.call(tilt);
+//    $('.project-display').animate({
+//        visibility: 'visible',
+//        top: '0px'
+//    }, 600, 'easeInOutCirc', function(){
+//        
+//    });
+    
+}
+
+$(document).ready(function() {
+    var isMobile = {
+			    Android: function() {
+			        return navigator.userAgent.match(/Android/i);
+			    },
+			    BlackBerry: function() {
+			        return navigator.userAgent.match(/BlackBerry/i);
+			    },
+			    iOS: function() {
+			        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+			    },
+			    Opera: function() {
+			        return navigator.userAgent.match(/Opera Mini/i);
+			    },
+			    Windows: function() {
+			        return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+			    },
+			    any: function() {
+			        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+			    }
+			};
+    $('.cross-button').click(function(){
+        console.log('cross clicked');
+        var projectDisplay = anime({
+            targets: '.project-display',
+            duration: 600,
+            top: '100vh',
+            easing: 'easeInOutCirc'
+        });
+    });
+    
+    var md = new MobileDetect(window.navigator.userAgent);
+    
+    console.log(md.mobile());
+    
+    if(md.mobile()){
+        var tiles = document.getElementsByClassName('project-tile');
+        for (var i=0; i<tiles.length; i++){
+            tiles[i].vanillaTilt.destroy();
+        }
+    }
+    
+});
