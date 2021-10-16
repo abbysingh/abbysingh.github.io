4180539e533f197fb37320325d21a5280cbea0e2
Abheyraj <abheyraj@mybosque.com>
2021-10-16 12:36:57 +0200

    Updated vertical scroll behaviour

--- src/components/container.js
+++ src/components/container.js
@@ -1,6 +1,26 @@
-import React from "react"
-import * as containerStyles from "./container.module.css"
+import React from "react";
+import * as containerStyles from "./container.module.css";
 
 export default function Container({ children }) {
-  return <div className={containerStyles.container}>{children}</div>
+  return (
+    <div
+      id="container"
+      className={containerStyles.container}
+      onWheel={handleOnWheel}
+    >
+      {children}
+    </div>
+  );
+}
+
+function handleOnWheel(e) {
+  e.preventDefault();
+  var containerScrollPosition = window.pageXOffset; //apparently the window is the container
+  console.log("container scroll Left " + containerScrollPosition);
+  var scrollDestination = containerScrollPosition + e.deltaY;
+  window.scrollTo({
+    top: 0,
+    left: scrollDestination,
+    behaviour: "smooth",
+  });
 }
